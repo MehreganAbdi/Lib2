9188a79aca0c4427a28d8ba7e0e085cc38f43b47
Mehregan Abdi <mehreganabdix@gmail.com>
2023-08-13 00:52:38 -0700

    Reiceive Methode has a bug in libOps

--- LibraryTests/LibraryOperationsTests.cs
+++ LibraryTests/LibraryOperationsTests.cs
@@ -22,37 +22,47 @@ namespace LibraryTests
             Assert.True(_LibraryOperators.IsBan(mem));
         }
 
-        //[Fact]
-        //public void Loan_AddingToLoanListsTest_CheckingListsManually()
-        //{
-        //    var memb1518 = new _Member("John", 1518);
-        //    var testBook1518 = new _Book("Travis", 2111, 3);
+        [Fact]
+        public void Looan_SearchingDataListsManually()
+        {
+            var book9897 = new _Book("Book9897", 9897, 10);
+            var mem9897 = new _Member("Scott9897", 98972);
 
-        //    BookOperations.AddThis(testBook1518);
-        //    MemberOperations.AddThis(memb1518);
+            MemberOperations.AddThis(mem9897);
+            BookOperations.AddThis(book9897);
 
+            _LibraryOperators.Loan(mem9897, book9897);
+            bool x = _Data.LoanedBooks.Contains(book9897) && _Data.Loaned_Members.Contains(mem9897);
 
-        //    _LibraryOperators.Loan(memb1518, testBook1518);
+            Assert.True(x);
+        }
+        [Fact]
+        public void Recieve_SearchinDatalistsManually()
+        {
+            var book9896 = new _Book("Book9896", 9896, 10);
+            var mem9896 = new _Member("Scott9896", 98962);
 
-        //    Assert.True(_Data.LoanedBooks.Contains(testBook1518) && _Data.Loaned_Members.Contains(memb1518));
-        //}
+            MemberOperations.AddThis(mem9896);
+            BookOperations.AddThis(book9896);
 
-        //[Fact]
-        //public void Recieve_CheckListsManually()
-        //{
-        //    var validMember = new _Member("John", 1818);
-        //    var testBook = new _Book("Travis", 1111, 3);
-
-        //    BookOperations.AddThis(testBook);
-        //    MemberOperations.AddThis(validMember);
-
-
-        //    _LibraryOperators.Loan(validMember, testBook);
-        //    _LibraryOperators.Receive(testBook, validMember);
+            _Data.Loaned_Members.Add(mem9896);
+            _Data.LoanedBooks.Add(book9896);
 
 
-        //    Assert.True(!_Data.LoanedBooks.Contains(testBook) && !_Data.Loaned_Members.Contains(validMember));
+            var book9895 = new _Book("Book9895", 9895, 10);
+            var mem9895 = new _Member("Scott9895", 98952);
 
-        //}
+            MemberOperations.AddThis(mem9895);
+            BookOperations.AddThis(book9895);
+
+            _Data.Loaned_Members.Add(mem9895);
+            _Data.LoanedBooks.Add(book9895);
+
+            _LibraryOperators.Receive(book9895, mem9895);
+
+            bool x = _Data.LoanedBooks.Contains(book9895) &&_Data.Loaned_Members.Contains(mem9895);
+
+            Assert.False(x);
+        }
     }
 }
